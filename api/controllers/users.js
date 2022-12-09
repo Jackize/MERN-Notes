1155ba6ead4f88a5af74812a23628371a3a3eb9a
Jackize <nguyenthanhhao2042002@Gmail.com>
2022-12-10 00:42:08 +0700

    Admin user and user disabling

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { Note } = require('../models');
-const { User } = require('../models');
+
+const { tokenExtractor } = require('../util/middleware');
+const { Note, User } = require('../models');
 
 router.get('/', async (req, res) => {
     const users = await User.findAll({
@@ -24,6 +25,35 @@ router.post('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     const user = await User.findByPk(req.params.id);
     if (user) {
+        res.json(user);
+    } else {
+        res.status(404).end();
+    }
+});
+
+const isAdmin = async (req, res, next) => {
+    const user = await User.findByPk(req.decodedToken.id, {
+        raw: true,
+    });
+    if (!user.admin) {
+        return res.status(401).json({ error: 'operation not allowed' });
+    }
+    next();
+};
+router.put('/:id', tokenExtractor, isAdmin, async (req, res) => {
+    let user = await User.findByPk(req.params.id, {
+        raw: true,
+    });
+    if (user) {
+        await User.update(
+            { ...req.body },
+            {
+                where: {
+                    id: user.id,
+                },
+            }
+        );
+        user = { ...user, ...req.body };
         res.json(user);
     } else {
         res.status(404).end();
