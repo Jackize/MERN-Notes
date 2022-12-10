5521cd8ca2ef5bf1d5705a3a5d2698cd968deb63
Jackize <nguyenthanhhao2042002@Gmail.com>
2022-12-10 14:36:18 +0700

    Many-to-many relationships

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,14 +1,23 @@
 const router = require('express').Router();
 
 const { tokenExtractor } = require('../util/middleware');
-const { Note, User } = require('../models');
+const { Note, User, Team } = require('../models');
 
 router.get('/', async (req, res) => {
     const users = await User.findAll({
-        include: {
-            model: Note,
-            attributes: { exclude: ['userId'] },
-        },
+        include: [
+            {
+                model: Note,
+                attributes: { exclude: ['userId'] },
+            },
+            {
+                model: Team,
+                attributes: ['name', 'id'],
+                through: {
+                    attributes: [],
+                },
+            },
+        ],
     });
     res.json(users);
 });
--- api/models/index.js
+++ api/models/index.js
@@ -1,12 +1,18 @@
 const Note = require('./note');
 const User = require('./user');
+const Team = require('./team');
+const Membership = require('./membership');
 
 User.hasMany(Note);
 Note.belongsTo(User);
+User.belongsToMany(Team, { through: Membership });
+Team.belongsToMany(User, { through: Membership });
 Note.sync({ alter: true });
 User.sync({ alter: true });
 
 module.exports = {
     Note,
     User,
+    Team,
+    Membership,
 };
