92a8d4a56375e0b076217278c3668662131f522e
Jackize <nguyenthanhhao2042002@Gmail.com>
2022-12-10 15:36:19 +0700

    Revisting many-to-many realtionships

--- api/models/index.js
+++ api/models/index.js
@@ -2,17 +2,24 @@ const Note = require('./note');
 const User = require('./user');
 const Team = require('./team');
 const Membership = require('./membership');
+const UserNotes = require('./user_notes');
 
 User.hasMany(Note);
 Note.belongsTo(User);
+
 User.belongsToMany(Team, { through: Membership });
 Team.belongsToMany(User, { through: Membership });
-Note.sync({ alter: true });
-User.sync({ alter: true });
+
+User.belongsToMany(Note, { through: UserNotes, as: 'marked_notes' });
+Note.belongsToMany(User, { through: UserNotes, as: 'users_marked' });
+
+Note.sync();
+User.sync();
 
 module.exports = {
     Note,
     User,
     Team,
     Membership,
+    UserNotes,
 };
