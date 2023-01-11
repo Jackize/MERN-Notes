24fb2856915996d94ae93648c30e9e9d6c70fb0d
Jackize <nguyenthanhhao2042002@Gmail.com>
2023-01-12 00:23:49 +0700

    Part 5: Refactor & Subscriptions server

--- server/index.js
+++ server/index.js
@@ -1,177 +1,75 @@
 require("dotenv").config();
-const {
-  ApolloServer,
-  gql,
-  UserInputError,
-  AuthenticationError,
-} = require("apollo-server");
-const { v1: uuid } = require("uuid");
-const { connectDB } = require("./connectDB");
+const { ApolloServer } = require("apollo-server-express");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
+const express = require("express");
+const http = require("http");
+const { execute, subscribe } = require("graphql");
+const { WebSocketServer } = require("ws");
+const { useServer } = require("graphql-ws/lib/use/ws");
 const jwt = require("jsonwebtoken");
+
+const { connectDB } = require("./connectDB");
 const User = require("./models/user");
-const Person = require("./models/person");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+const bodyParser = require("body-parser");
 
 const JWT_SECRET = process.env.JWT_SECRET;
-const typeDefs = gql`
-  type Address {
-    street: String!
-    city: String!
-  }
-
-  type Person {
-    name: String!
-    phone: String
-    address: Address!
-    id: ID!
-  }
-
-  enum YesNo {
-    YES
-    NO
-  }
-
-  type User {
-    username: String!
-    friends: [Person!]!
-    id: ID!
-  }
-
-  type Token {
-    value: String!
-  }
-
-  type Query {
-    personCount: Int!
-    allPersons(phone: YesNo): [Person!]!
-    findPerson(name: String!): Person
-    me: User
-  }
-
-  type Mutation {
-    addPerson(
-      name: String!
-      phone: String
-      street: String!
-      city: String!
-    ): Person
-    editNumber(name: String!, phone: String!): Person
-    createUser(username: String!): User
-    login(username: String!, password: String!): Token
-    addAsFriend(name: String!): User
-  }
-`;
 
 connectDB();
-const resolvers = {
-  Query: {
-    personCount: async () => Person.collection.countDocuments(),
-    allPersons: async (root, args) => {
-      if (!args.phone) return Person.find({});
 
-      return Person.find({ phone: { $exists: args.phone === "YES" } });
-    },
-    findPerson: async (root, args) => Person.findOne({ name: args.name }),
-    me: (root, args, context) => context.currentUser,
-  },
-  Person: {
-    address: (root) => {
-      return {
-        street: root.street,
-        city: root.city,
-      };
-    },
-  },
-  Mutation: {
-    addPerson: async (root, args, context) => {
-      const person = new Person({ ...args });
-      const currentUser = context.currentUser;
-      if (!currentUser) {
-        throw new AuthenticatorAssertionResponseError("not authenticated");
-      }
+const start = async () => {
+  const app = express();
+  const httpServer = http.createServer(app);
 
-      try {
-        await person.save();
-        currentUser.friends = currentUser.friends.concat(person);
-        await currentUser.save();
-      } catch (error) {
-        throw new UserInputError(error.message, {
-          invalidArgs: args,
-        });
-      }
-      return person;
-    },
-    editNumber: async (root, args) => {
-      const person = await Person.findOne({ name: args.name });
-      person.phone = args.phone;
-      try {
-        await person.save();
-      } catch (error) {
-        throw new UserInputError(error.message, {
-          invalidArgs: args,
-        });
-      }
-      return person;
-    },
-    createUser: async (root, args) => {
-      const user = new User({ username: args.username });
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-      return user.save().catch((err) => {
-        throw new UserInputError(err.message, {
-          invalidArgs: args,
-        });
-      });
-    },
-    login: async (root, args) => {
-      const user = await User.findOne({ username: args.username });
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: "/",
+  });
 
-      if (!user || args.password !== "secret") {
-        throw new UserInputError("Wrong credentials");
-      }
+  const serverCleanup = useServer({ schema }, wsServer);
 
-      const userForToken = {
-        username: user.username,
-        id: user._id,
-      };
-
-      return { value: jwt.sign(userForToken, JWT_SECRET) };
-    },
-    addAsFriend: async (root, args, { currentUser }) => {
-      const isFriend = (person) =>
-        currentUser.friends.map((f) =>
-          f._id.toString().includes(person._id.toString())
+  const server = new ApolloServer({
+    schema,
+    context: async ({ req }) => {
+      const auth = req ? req.headers.authorization : null;
+      if (auth && auth.toLowerCase().startsWith("bearer ")) {
+        const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET);
+        const currentUser = await User.findById(decodedToken.id).populate(
+          "friends"
         );
-
-      if (!currentUser) {
-        throw new AuthenticationError("not authenticated");
+        return { currentUser };
       }
-
-      const person = await Person.findOne({ name: args.name });
-      if (!isFriend(person)) {
-        currentUser.friends = currentUser.friends.concat(person);
-      }
-
-      await currentUser.save();
-      return currentUser;
     },
-  },
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
+    ],
+  });
+
+  await server.start();
+
+  server.applyMiddleware({
+    app,
+    path: "/",
+  });
+
+  const PORT = 4000;
+
+  httpServer.listen(PORT, () =>
+    console.log(`Server is now running on http://localhost:${PORT}`)
+  );
 };
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: async ({ req }) => {
-    const auth = req ? req.headers.authorization : null;
-    // console.log(req.headers.authorization);
-    if (auth && auth.toLowerCase().startsWith("bearer ")) {
-      const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET);
-      const currentUser = await User.findById(decodedToken.id).populate(
-        "friends"
-      );
-      return { currentUser };
-    }
-  },
-});
-
-server.listen().then(({ url = 3001 }) => {
-  console.log(`Server ready at ${url}`);
-});
+start();
